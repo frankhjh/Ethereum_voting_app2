ea19fb5d8687688fef5fa99a8b07ced855713835
frankhjh <73477765+frankhjh@users.noreply.github.com>
2021-05-26 12:31:29 +0800

    Update flask_sorting.py

--- flask_sorting.py
+++ flask_sorting.py
@@ -1,13 +1,18 @@
 from conn import w3,sorting_contract
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,send_from_directory
 import json
 import time
+import os
 
 vote_app2=Flask(__name__)
 
 @vote_app2.route('/')
 def welcome():
     return render_template('content.html')
+
+@vote_app2.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(vote_app2.root_path,'static'),'favicon.ico')
 
 @vote_app2.route('/<filename>')
 def ballot_status(filename):
